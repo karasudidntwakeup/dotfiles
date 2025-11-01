db0175f7fd70f2c28423d8ce9bffd6cd6b59d633
whitesector <karasu ?>
2025-11-01 15:54:57 +0200

    wall

--- .config/mango/autostart.sh
+++ .config/mango/autostart.sh
@@ -1,3 +1,3 @@
 waybar -c ~/.config/mango/config.jsonc -s ~/.config/mango/style.css >/dev/null 2>&1 &
-swaybg -i ~/wallpaper/1.PNG >/dev/null 2>&1 &
+swaybg -i ~/wallpaper/dandadan-anime-momo-4k-wallpaper-uhdpaper.com-68@2@b.jpg >/dev/null 2>&1 &
 pipewire & pipewire-pulse & wireplumber &   
