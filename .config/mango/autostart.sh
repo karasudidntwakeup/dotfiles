0b96207fe6b39d68a2c6cebddd33c56150190ea3
whitesector <karasu ?>
2025-11-01 14:03:07 +0200

    mangotest

--- .config/mango/autostart.sh
+++ .config/mango/autostart.sh
@@ -1,3 +1,3 @@
 waybar -c ~/.config/mango/config.jsonc -s ~/.config/mango/style.css >/dev/null 2>&1 &
-swaybg -i ~/walls/wall3.jpg >/dev/null 2>&1 &
-
+swaybg -i ~/wallpaper/1.PNG >/dev/null 2>&1 &
+exec-once =  pipewire & pipewire-pulse & wireplumber   
