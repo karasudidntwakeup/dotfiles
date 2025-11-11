5a0fd5bc898ad941b78e5f588c8d3a0637726d96
whitesector <karasu ?>
2025-11-11 20:49:08 +0200

    KindaNiri

---
+++ .config/niri/autostart.sh
@@ -0,0 +1,3 @@
+waybar -c ~/.config/mango/config.jsonc -s ~/.config/mango/style.css >/dev/null 2>&1 &
+swaybg -i ~/wallpaper/wallhaven-4x22vz.jpg -m center >/dev/null 2>&1 &
+pipewire & pipewire-pulse & wireplumber &   
