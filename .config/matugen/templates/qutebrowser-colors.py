a50a39023010225a09c59815c28298b172f7afba
karasu <hemdanoovic@gmail.com>
2026-07-21 19:45:36 +0300

    qute

---
+++ .config/matugen/templates/qutebrowser-colors.py
@@ -0,0 +1,24 @@
+c.colors.completion.fg = "{{colors.on_surface.default.hex}}"
+c.colors.completion.odd.bg = "{{colors.surface.default.hex}}"
+c.colors.completion.even.bg = "{{colors.surface_variant.default.hex}}"
+c.colors.completion.category.fg = "{{colors.primary.default.hex}}"
+c.colors.completion.category.bg = "{{colors.surface.default.hex}}"
+c.colors.completion.item.selected.fg = "{{colors.on_primary.default.hex}}"
+c.colors.completion.item.selected.bg = "{{colors.primary.default.hex}}"
+c.colors.completion.match.fg = "{{colors.tertiary.default.hex}}"
+
+c.colors.statusbar.normal.fg = "{{colors.on_surface.default.hex}}"
+c.colors.statusbar.normal.bg = "{{colors.surface.default.hex}}"
+c.colors.statusbar.insert.bg = "{{colors.primary.default.hex}}"
+c.colors.statusbar.command.bg = "{{colors.surface_variant.default.hex}}"
+
+c.colors.tabs.bar.bg = "{{colors.surface.default.hex}}"
+c.colors.tabs.odd.fg = "{{colors.on_surface.default.hex}}"
+c.colors.tabs.odd.bg = "{{colors.surface_variant.default.hex}}"
+c.colors.tabs.even.fg = "{{colors.on_surface.default.hex}}"
+c.colors.tabs.even.bg = "{{colors.surface_variant.default.hex}}"
+c.colors.tabs.selected.odd.fg = "{{colors.on_primary.default.hex}}"
+c.colors.tabs.selected.odd.bg = "{{colors.primary.default.hex}}"
+
+c.colors.webpage.bg = "{{colors.surface.default.hex}}"
+c.colors.webpage.darkmode.enabled = True
