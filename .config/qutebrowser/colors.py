309b1245638f31a1c91bfef4ef37c9f472db22a2
karasu <hemdanoovic@gmail.com>
2026-07-21 19:45:22 +0300

    qute

---
+++ .config/qutebrowser/colors.py
@@ -0,0 +1,24 @@
+c.colors.completion.fg = "#e9e0e7"
+c.colors.completion.odd.bg = "#161217"
+c.colors.completion.even.bg = "#4c444d"
+c.colors.completion.category.fg = "#e3b7f3"
+c.colors.completion.category.bg = "#161217"
+c.colors.completion.item.selected.fg = "#442254"
+c.colors.completion.item.selected.bg = "#e3b7f3"
+c.colors.completion.match.fg = "#f5b7b5"
+
+c.colors.statusbar.normal.fg = "#e9e0e7"
+c.colors.statusbar.normal.bg = "#161217"
+c.colors.statusbar.insert.bg = "#e3b7f3"
+c.colors.statusbar.command.bg = "#4c444d"
+
+c.colors.tabs.bar.bg = "#161217"
+c.colors.tabs.odd.fg = "#e9e0e7"
+c.colors.tabs.odd.bg = "#4c444d"
+c.colors.tabs.even.fg = "#e9e0e7"
+c.colors.tabs.even.bg = "#4c444d"
+c.colors.tabs.selected.odd.fg = "#442254"
+c.colors.tabs.selected.odd.bg = "#e3b7f3"
+
+c.colors.webpage.bg = "#161217"
+c.colors.webpage.darkmode.enabled = True
