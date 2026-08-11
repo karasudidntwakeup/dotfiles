66c2aad9bba12ac6b257de1bc4f72d4b17744c85
karasu <hemdanoovic@gmail.com>
2026-08-11 18:11:16 +0300

    fixes

--- .config/quickshell/colors.js
+++ .config/quickshell/colors.js
@@ -4,103 +4,103 @@
 */
 
 
-var background = "#0f1417"
+var background = "#11140f"
 
 var error = "#ffb4ab"
 
 var error_container = "#93000a"
 
-var inverse_on_surface = "#2c3134"
+var inverse_on_surface = "#2e322b"
 
-var inverse_primary = "#196585"
+var inverse_primary = "#406835"
 
-var inverse_surface = "#dfe3e7"
+var inverse_surface = "#e1e4da"
 
-var on_background = "#dfe3e7"
+var on_background = "#e1e4da"
 
 var on_error = "#690005"
 
 var on_error_container = "#ffdad6"
 
-var on_primary = "#003548"
+var on_primary = "#12380b"
 
-var on_primary_container = "#c2e8ff"
+var on_primary_container = "#c1efaf"
 
-var on_primary_fixed = "#001e2c"
+var on_primary_fixed = "#012200"
 
-var on_primary_fixed_variant = "#004d67"
+var on_primary_fixed_variant = "#294f20"
 
-var on_secondary = "#20333d"
+var on_secondary = "#273422"
 
-var on_secondary_container = "#d1e5f3"
+var on_secondary_container = "#d7e8cc"
 
-var on_secondary_fixed = "#091e28"
+var on_secondary_fixed = "#121f0e"
 
-var on_secondary_fixed_variant = "#364954"
+var on_secondary_fixed_variant = "#3d4b37"
 
-var on_surface = "#dfe3e7"
+var on_surface = "#e1e4da"
 
-var on_surface_variant = "#c0c7cd"
+var on_surface_variant = "#c3c8bc"
 
-var on_tertiary = "#312c4c"
+var on_tertiary = "#003739"
 
-var on_tertiary_container = "#e5deff"
+var on_tertiary_container = "#bcebee"
 
-var on_tertiary_fixed = "#1c1736"
+var on_tertiary_fixed = "#002021"
 
-var on_tertiary_fixed_variant = "#484364"
+var on_tertiary_fixed_variant = "#1e4d50"
 
-var outline = "#8a9297"
+var outline = "#8d9387"
 
-var outline_variant = "#40484d"
+var outline_variant = "#43483f"
 
-var primary = "#8ecff2"
+var primary = "#a6d395"
 
-var primary_container = "#004d67"
+var primary_container = "#294f20"
 
-var primary_fixed = "#c2e8ff"
+var primary_fixed = "#c1efaf"
 
-var primary_fixed_dim = "#8ecff2"
+var primary_fixed_dim = "#a6d395"
 
 var scrim = "#000000"
 
-var secondary = "#b5c9d7"
+var secondary = "#bbcbb1"
 
-var secondary_container = "#364954"
+var secondary_container = "#3d4b37"
 
-var secondary_fixed = "#d1e5f3"
+var secondary_fixed = "#d7e8cc"
 
-var secondary_fixed_dim = "#b5c9d7"
+var secondary_fixed_dim = "#bbcbb1"
 
 var shadow = "#000000"
 
-var source_color = "#86c6e9"
+var source_color = "#88aa7b"
 
-var surface = "#0f1417"
+var surface = "#11140f"
 
-var surface_bright = "#353a3d"
+var surface_bright = "#363a34"
 
-var surface_container = "#1b2023"
+var surface_container = "#1d211b"
 
-var surface_container_high = "#262b2e"
+var surface_container_high = "#272b25"
 
-var surface_container_highest = "#313539"
+var surface_container_highest = "#32362f"
 
-var surface_container_low = "#171c1f"
+var surface_container_low = "#191d17"
 
-var surface_container_lowest = "#0a0f12"
+var surface_container_lowest = "#0c0f0a"
 
-var surface_dim = "#0f1417"
+var surface_dim = "#11140f"
 
-var surface_tint = "#8ecff2"
+var surface_tint = "#a6d395"
 
-var surface_variant = "#40484d"
+var surface_variant = "#43483f"
 
-var tertiary = "#c9c1ea"
+var tertiary = "#a0cfd2"
 
-var tertiary_container = "#484364"
+var tertiary_container = "#1e4d50"
 
-var tertiary_fixed = "#e5deff"
+var tertiary_fixed = "#bcebee"
 
-var tertiary_fixed_dim = "#c9c1ea"
+var tertiary_fixed_dim = "#a0cfd2"
 
--- .config/quickshell/shell.qml
+++ .config/quickshell/shell.qml
@@ -12,7 +12,7 @@ import "colors.js" as Matugen
 // Mirrors ~/.config/waybar (modules + Material You pill styling).
 // Palette comes from matugen via colors.js (regenerated on wallpaper change).
 
-// matugen 1786390380
+// matugen 1786458817
 
 ShellRoot {
     id: root
