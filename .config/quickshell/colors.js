f420df156fe64cb16679ef6d2b0a5e4ebce0c01d
karasu <hemdanoovic@gmail.com>
2026-08-20 00:22:56 +0300

    fixes

--- .config/quickshell/colors.js
+++ .config/quickshell/colors.js
@@ -4,103 +4,103 @@
 */
 
 
-var background = "#0f1512"
+var background = "#131318"
 
 var error = "#ffb4ab"
 
 var error_container = "#93000a"
 
-var inverse_on_surface = "#2b322f"
+var inverse_on_surface = "#313036"
 
-var inverse_primary = "#106b57"
+var inverse_primary = "#5c5891"
 
-var inverse_surface = "#dee4e0"
+var inverse_surface = "#e5e1e9"
 
-var on_background = "#dee4e0"
+var on_background = "#e5e1e9"
 
 var on_error = "#690005"
 
 var on_error_container = "#ffdad6"
 
-var on_primary = "#00382c"
+var on_primary = "#2e295f"
 
-var on_primary_container = "#a2f2d9"
+var on_primary_container = "#e4dfff"
 
-var on_primary_fixed = "#002019"
+var on_primary_fixed = "#19124a"
 
-var on_primary_fixed_variant = "#005141"
+var on_primary_fixed_variant = "#454078"
 
-var on_secondary = "#1d352e"
+var on_secondary = "#302e41"
 
-var on_secondary_container = "#cde9de"
+var on_secondary_container = "#e4dff9"
 
-var on_secondary_fixed = "#072019"
+var on_secondary_fixed = "#1b192c"
 
-var on_secondary_fixed_variant = "#344c44"
+var on_secondary_fixed_variant = "#474459"
 
-var on_surface = "#dee4e0"
+var on_surface = "#e5e1e9"
 
-var on_surface_variant = "#bfc9c4"
+var on_surface_variant = "#c9c5d0"
 
-var on_tertiary = "#0e3446"
+var on_tertiary = "#482537"
 
-var on_tertiary_container = "#c4e7ff"
+var on_tertiary_container = "#ffd8e8"
 
-var on_tertiary_fixed = "#001e2d"
+var on_tertiary_fixed = "#2f1122"
 
-var on_tertiary_fixed_variant = "#284b5e"
+var on_tertiary_fixed_variant = "#613b4e"
 
-var outline = "#89938e"
+var outline = "#928f99"
 
-var outline_variant = "#3f4945"
+var outline_variant = "#47464f"
 
-var primary = "#87d6bd"
+var primary = "#c6c0ff"
 
-var primary_container = "#005141"
+var primary_container = "#454078"
 
-var primary_fixed = "#a2f2d9"
+var primary_fixed = "#e4dfff"
 
-var primary_fixed_dim = "#87d6bd"
+var primary_fixed_dim = "#c6c0ff"
 
 var scrim = "#000000"
 
-var secondary = "#b2ccc2"
+var secondary = "#c8c3dc"
 
-var secondary_container = "#344c44"
+var secondary_container = "#474459"
 
-var secondary_fixed = "#cde9de"
+var secondary_fixed = "#e4dff9"
 
-var secondary_fixed_dim = "#b2ccc2"
+var secondary_fixed_dim = "#c8c3dc"
 
 var shadow = "#000000"
 
-var source_color = "#83918b"
+var source_color = "#424052"
 
-var surface = "#0f1512"
+var surface = "#131318"
 
-var surface_bright = "#343b38"
+var surface_bright = "#3a383e"
 
-var surface_container = "#1b211e"
+var surface_container = "#201f25"
 
-var surface_container_high = "#252b29"
+var surface_container_high = "#2a292f"
 
-var surface_container_highest = "#303633"
+var surface_container_highest = "#35343a"
 
-var surface_container_low = "#171d1b"
+var surface_container_low = "#1c1b20"
 
-var surface_container_lowest = "#090f0d"
+var surface_container_lowest = "#0e0e13"
 
-var surface_dim = "#0f1512"
+var surface_dim = "#131318"
 
-var surface_tint = "#87d6bd"
+var surface_tint = "#c6c0ff"
 
-var surface_variant = "#3f4945"
+var surface_variant = "#47464f"
 
-var tertiary = "#a9cbe2"
+var tertiary = "#ebb8cf"
 
-var tertiary_container = "#284b5e"
+var tertiary_container = "#613b4e"
 
-var tertiary_fixed = "#c4e7ff"
+var tertiary_fixed = "#ffd8e8"
 
-var tertiary_fixed_dim = "#a9cbe2"
+var tertiary_fixed_dim = "#ebb8cf"
 
