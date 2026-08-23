a8ddafac96aac9a4472f9b3b0a24ffcb1c8597e1
karasu <hemdanoovic@gmail.com>
2026-08-24 02:21:50 +0300

    fix

--- .config/matugen/templates/quickshell-colors.js
+++ .config/matugen/templates/quickshell-colors.js
@@ -3,6 +3,28 @@
  * Generated with Matugen
  */
 
+/* Scheme colors (current mode) */
+
 <* for name, value in colors *>
 var {{name}} = "{{value.default.hex}}"
+<* endfor *>
+
+/* Light scheme variants */
+
+<* for name, value in colors *>
+var {{name}}_light = "{{value.light.hex}}"
+<* endfor *>
+
+/* Dark scheme variants */
+
+<* for name, value in colors *>
+var {{name}}_dark = "{{value.dark.hex}}"
+<* endfor *>
+
+/* Tonal palettes (tones 0-100) */
+
+<* for pname, pal in palettes *>
+<* for tone, tval in pal *>
+var palette_{{pname}}_{{tone}} = "{{tval.hex}}"
+<* endfor *>
 <* endfor *>
