0d5e0cb0f26775ce4645a58c5b5c6b868ac65a51
karasu <hemdanoovic@gmail.com>
2026-09-01 18:40:33 +0300

    remove slops

--- .config/quickshell/scripts/network.sh
+++ .config/quickshell/scripts/network.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Show the local IP address of the connected network interface
-IP=$(ip -4 route get 1.1.1.1 2>/dev/null | grep -oP 'src \K\S+')
+IP=$(ip -4 route get 1.1.1.1 2>/dev/null | awk '{for(i=1;i<=NF;i++) if($i=="src") print $(i+1)}')
 
 if [ -z "$IP" ]; then
     echo "󰖪  "
