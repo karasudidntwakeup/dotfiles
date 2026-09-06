3b35fa6c3df7dfb8a324538c942f8e79eec9e784
karasu <hemdanoovic@gmail.com>
2026-09-06 07:52:10 +0300

    remove slops

--- .config/quickshell/colors.js
+++ .config/quickshell/colors.js
@@ -5,53 +5,53 @@
 
 /* Base scheme colors used by the shell */
 
-var primary = "#def5ee"
+var primary = "#bdffd1"
 
-var primary_container = "#b1c7c1"
+var primary_container = "#76d69a"
 
-var primary_fixed_dim = "#b5cbc5"
+var primary_fixed_dim = "#7ada9e"
 
 var error = "#ffece9"
 
 var error_container = "#ffaea4"
 
-var outline_variant = "#bec4c1"
+var outline_variant = "#c2c2c2"
 
 var shadow = "#000000"
 
 var on_primary_container = "#000000"
 
-var secondary_container = "#bcc4c1"
+var secondary_container = "#b2c8b6"
 
-var secondary_fixed = "#dce4e1"
+var secondary_fixed = "#d1e8d5"
 
-var secondary_fixed_dim = "#c0c8c5"
+var secondary_fixed_dim = "#b5ccb9"
 
-var tertiary_container = "#c1c1d6"
+var tertiary_container = "#9fc9d7"
 
-var tertiary_fixed_dim = "#c5c5da"
+var tertiary_fixed_dim = "#a3cddb"
 
 /* Light scheme variants (QS "Light" mode pill backgrounds) */
 
-var primary_container_light = "#1b2f2b"
+var primary_container_light = "#005530"
 
-var primary_fixed_dim_light = "#223632"
+var primary_fixed_dim_light = "#003b20"
 
-var secondary_container_light = "#434b49"
+var secondary_container_light = "#3a4e3f"
 
-var secondary_fixed_light = "#434b49"
+var secondary_fixed_light = "#3a4e3f"
 
-var secondary_fixed_dim_light = "#2c3432"
+var secondary_fixed_dim_light = "#24372a"
 
-var tertiary_container_light = "#292a3a"
+var tertiary_container_light = "#244f5a"
 
-var tertiary_fixed_dim_light = "#303142"
+var tertiary_fixed_dim_light = "#063843"
 
 /* Custom widget colors */
-var prayer = "#bcc4c1"
+var prayer = "#b2c8b6"
 
-var prayer_light = "#434b49"
+var prayer_light = "#3a4e3f"
 
-var battery = "#b1c7c1"
+var battery = "#76d69a"
 
-var battery_light = "#1b2f2b"
+var battery_light = "#005530"
